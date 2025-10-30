bf62a4bfaa7f0d91ad3c8e73c4db44414d438f9e
Abdur Rahman <Abdurrahmankazi199@gmail.com>
2025-10-30 16:49:28 +0600

    Update BootReceiver.java

--- app/src/main/java/com/abdurrahman/toolsforandroid/BootReceiver.java
+++ app/src/main/java/com/abdurrahman/toolsforandroid/BootReceiver.java
@@ -9,17 +9,30 @@ import com.abdurrahman.toolsforandroid.services.SensorService;
 import com.abdurrahman.toolsforandroid.utils.SharedPrefs;
 
 public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG = "BootReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) || 
+            "android.intent.action.QUICKBOOT_POWERON".equals(intent.getAction())) {
+            
             SharedPrefs prefs = new SharedPrefs(context);
             
             // Start service if any background feature is enabled
             if (prefs.isShakeFlashlightEnabled() || prefs.isAutoSilentEnabled()) {
                 Intent serviceIntent = new Intent(context, SensorService.class);
-                context.startService(serviceIntent);
-                Log.d("BootReceiver", "SensorService started on boot");
+                try {
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                        context.startForegroundService(serviceIntent);
+                    } else {
+                        context.startService(serviceIntent);
+                    }
+                    Log.d(TAG, "SensorService started on boot");
+                } catch (SecurityException e) {
+                    Log.e(TAG, "Security exception starting service on boot", e);
+                } catch (Exception e) {
+                    Log.e(TAG, "Error starting service on boot", e);
+                }
             }
         }
     }
